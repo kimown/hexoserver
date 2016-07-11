6965a789a5f12902e4c5224076d9391ef54eb748
kimown <kimown@163.com>
2016-07-11 20:43:26 +0800

    dependency update

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # hexoserver
 a server for hexo ,using webpack-dev-server, react-hot
+
+
+
+HOW TO USE:
+
+
+```
+cd /tmp
+hexo init blog
+cd blog
+hexo g
+npm install hexoserver-cli -g
+hexoserver
+```
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.md$/,
-                loader: "html!markdown-hexo"
+                loader: "html!markdown-hexo-loader"
             },
             {
                 test: /\.js$/,
